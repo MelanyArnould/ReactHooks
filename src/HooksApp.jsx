b3ef1cd390f4e2841f89ec8a095e3a09cdf08e4b
Arnould555 <melanyarnould@gmail.com>
2024-09-16 20:38:26 -0300

    CounterApp.jsx

---
+++ src/HooksApp.jsx
@@ -0,0 +1,15 @@
+import { CounterApp } from "./components/CounterApp"
+
+
+export const HooksApp = () => {
+  return (
+        <>
+            <h1>Aplicación de Hooks</h1>
+            <hr />
+            <h4>Vite + React + Bootstrap</h4>
+            <CounterApp/>
+
+
+        </>  
+    )
+}
---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,17 @@
+import { useCounter } from "../hooks/useCounter"
+
+export const CounterApp = () => {
+
+    const {counter,increment,reset,decrement} = useCounter(0)
+
+  return (
+    <>
+        <h1>Counter</h1>
+        <h4>{counter}</h4>
+        <button className="btn btn-outline-secondary" onClick={() => increment()}>+</button>
+        <button className="btn btn-outline-danger" onClick={() => reset()}>Reset</button>
+        <button className="btn btn-outline-secondary" onClick={() => decrement()}>- </button>
+
+    </>
+  )
+}
